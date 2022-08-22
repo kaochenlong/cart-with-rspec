048a4a940cb36c3b27afb52db6c9c3dcddf9b73f
Eddie <eddie@5xcampus.com>
2022-08-22 12:05:00 +0800

    add cartitem

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,8 +3,16 @@ class Cart
     @items = []
   end
 
-  def add(product_id)
-    @items << product_id
+  def add(product_id, quantity = 1)
+    found_item = @items.find { |item|
+      item.product_id == product_id
+    }
+
+    if found_item
+      found_item.increment(quantity)
+    else
+      @items << CartItem.new(product_id, quantity)
+    end
   end
 
   def empty?
